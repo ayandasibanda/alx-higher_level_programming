19c9bd45a72caadb18cacf941102682d0661b095
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-08-07 14:38:59 -0700

    Changes to file

--- 0x00-python-hello_world/0-run.py
+++ 0x00-python-hello_world/0-run.py
@@ -1,2 +1,17 @@
-#!/usr/bin/python3
-python main.py
+#!/bin/bash
+
+# Checks if the environment variable PYFILE is set
+if [ -z "$PYFILE" ]; then
+  echo "Error: The PYFILE environment variable has not been set."
+  exit 1
+fi
+
+# Checks if the specified Python file exists
+if [ ! -f "$PYFILE" ]; then
+  echo "Error: The specified Python file does not exist."
+  exit 1
+fi
+
+# Run the Python script
+python3 "$PYFILE"
+
