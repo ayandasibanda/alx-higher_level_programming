07a88916b6d508bc8e9214e3356834d1f76c626f
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-08-26 12:28:25 +0000

    adding files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end='')
-
-print()
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
