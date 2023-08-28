d3e2ba3be1509cedee24737a27490b8f5f66da34
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-08-28 19:58:55 +0000

    Adding files

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format() and handles exceptions.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if the value is printed successfully as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False
