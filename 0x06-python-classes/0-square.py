e5501a61415e6efe29eb95f233b036fb8871c0ef
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-08-30 17:11:31 +0000

    Adding files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Define a class Square.
+"""
+
+class Square:
+    """
+    Represent a square.
+    """
+    pass
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,13 @@
+import math
+
+class MagicClass:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return math.pi * (self.__radius ** 2)
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
